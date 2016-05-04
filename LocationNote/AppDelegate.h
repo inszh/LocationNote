27eb0c4d292bb99712c683a94fe5300b788fcfae
inszh <inszh@163.com>
2016-05-04 15:02:06 +0800

    Initial Commit

---
+++ LocationNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationNote
+//
+//  Created by 小华 on 16/5/4.
+//  Copyright © 2016年 ark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationNote
+//
+//  Created by 小华 on 16/5/4.
+//  Copyright © 2016年 ark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
